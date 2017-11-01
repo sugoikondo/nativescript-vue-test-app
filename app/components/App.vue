4943ca3963eccc312a7716608b93f79ee267fcad
atsu <atsu9615@me.com>
2017-11-01 18:42:41 +0900

    インデントを４から２に

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,42 +1,40 @@
 <template>
-    <page>
-        <action-bar title="{N}-Vue + Rollup"></action-bar>
-        <stack-layout>
-            <label>Count: {{ count }}</label>
+<page>
+  <action-bar title="{N}-Vue + Rollup"></action-bar>
+  <stack-layout>
+    <label>Count: {{ count }}</label>
 
-            <button @tap="count--">-1</button>
+    <button @tap="count--">-1</button>
 
-            <test-component style="margin-top: 40;"></test-component>
-        </stack-layout>
-    </page>
+    <test-component style="margin-top: 40;"></test-component>
+  </stack-layout>
+</page>
 </template>
 
 <script>
-    import TestComponent from './TestComponent.vue'
+import TestComponent from './TestComponent.vue'
 
-    export default {
+export default {
 
-        data() {
-            return {
-                count: 10
-            }
-        },
-
-        components: {
-            TestComponent
-        }
+  data() {
+    return {
+      count: 10
     }
+  },
+
+  components: {
+    TestComponent
+  }
+}
 </script>
 
 <style scoped>
-    label {
-        text-align: center;
-        font-size: 25;
-    }
+label {
+  text-align: center;
+  font-size: 25;
+}
 
-    Button {
-        background-color: #00caab;
-    }
-
-
+Button {
+  background-color: #00caab;
+}
 </style>
--- app/components/TestComponent.vue
+++ app/components/TestComponent.vue
@@ -1,30 +1,30 @@
 <template>
-    <stack-layout>
-        <label class="large">Test Component</label>
+<stack-layout>
+  <label class="large">Test Component</label>
 
-        <label>{{ $data }}</label>
+  <label>{{ $data }}</label>
 
-        <text-field v-model="name"></text-field>
-        <text-field v-model="age"></text-field>
-        <text-field v-model="occupation"></text-field>
-    </stack-layout>
+  <text-field v-model="name"></text-field>
+  <text-field v-model="age"></text-field>
+  <text-field v-model="occupation"></text-field>
+</stack-layout>
 </template>
 
 <script>
-    export default {
+export default {
 
-        data() {
-            return {
-                name: 'John',
-                age: 34,
-                occupation: 'Developer'
-            }
-        }
+  data() {
+    return {
+      name: 'John',
+      age: 34,
+      occupation: 'Developer'
     }
+  }
+}
 </script>
 
 <style scoped>
-    .large {
-        font-size: 40;
-    }
+.large {
+  font-size: 40;
+}
 </style>
